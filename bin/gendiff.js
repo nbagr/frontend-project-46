35b8dd741bedb728d335a4ce5a2f8065975f7af4
nbagr <nbagramov@gmail.com>
2023-03-02 23:01:36 +0300

    add option in gendiff helper

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,7 @@ const program = new Command();
 program
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .arguments('<filePath1> <filePath2> ')
 
 program.parse(process.argv);
