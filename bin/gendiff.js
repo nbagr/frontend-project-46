16bd7449f162160df8323dd7bc2b32544e3a20a3
nbagr <nbagramov@gmail.com>
2023-08-02 20:04:25 +0300

    fix options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,7 +10,7 @@ program
   .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filePath1> <filePath2>')
   .action((filePath1, filePath2) => {
-    console.log(gendiff(filePath1, filePath2));
+    console.log(gendiff(filePath1, filePath2, program.opts().format));
   });
 
 program.parse(process.argv);
